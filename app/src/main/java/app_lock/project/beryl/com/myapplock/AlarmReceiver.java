95a3ee8f5879f0899754168252c4c9c2feea5286
Shadab Azam <shadab.aazam@berylsystems.com>
2018-10-12 11:20:21 +0530

    init

---
+++ app/src/main/java/app_lock/project/beryl/com/myapplock/AlarmReceiver.java
@@ -0,0 +1,15 @@
+package app_lock.project.beryl.com.myapplock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by amitshekhar on 02/05/15.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, AppCheckServices.class));
+    }
+}
